49eb31d7bdb50c984c326d062d8395c54706fb2e
miguelbagotchay-sketch <miguelbagotchay@gmail.com>
2026-09-12 10:01:18 +0000

    Added working delete button for each task

--- script.js
+++ script.js
@@ -22,6 +22,15 @@ function renderTasks() {
   for (let i = 0; i < tasks.length; i++) {
     const li = document.createElement("li");
     li.textContent = tasks[i];
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.addEventListener("click", function () {
+      tasks.splice(i, 1);
+      renderTasks();
+    });
+
+    li.appendChild(deleteBtn);
     taskList.appendChild(li);
   }
 }
